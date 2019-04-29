518638593914cd604f7fddc6ae42d7495ebc4749
julianaaok <juliana.gh7@gmail.com>
2019-04-29 16:04:34 -0700

    popover y componentes en historial

--- ProyectoIntegrador/src/app/tab2/tab2.page.ts
+++ ProyectoIntegrador/src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { PopoverController } from '@ionic/angular';
+//import { PopoverComponent } from '';
 
 @Component({
   selector: 'app-tab2',
@@ -8,11 +10,19 @@ import { Router } from '@angular/router';
 })
 export class Tab2Page {
 
-  constructor(private router:Router){
+  constructor(private router:Router,public popoverController: PopoverController){}
 
+  async presentPopover(ev: any) {
+    const popover = await this.popoverController.create({
+      component: PopoverComponent,
+      event: ev,
+      translucent: true
+    });
+    return await popover.present();
   }
   
   back(){
     this.router.navigateByUrl("/app/tabs/tab1");
   }
 }
+
