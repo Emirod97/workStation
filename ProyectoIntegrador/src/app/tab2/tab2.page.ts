87fff52c647afc718a71eec3f9483710bf787c80
julianaaok <juliana.gh7@gmail.com>
2019-05-04 13:57:29 -0700

    popover works !

--- ProyectoIntegrador/src/app/tab2/tab2.page.ts
+++ ProyectoIntegrador/src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { PopoverController } from '@ionic/angular';
+import {PopoverComponent} from '../popover/popover.component';
 
 @Component({
   selector: 'app-tab2',
@@ -9,8 +11,8 @@ import { Router } from '@angular/router';
 export class Tab2Page {
 
 
-  constructor(private router:Router){}
-/*
+  constructor(private router:Router,public popoverController: PopoverController){}
+
   async presentPopover(ev: any) {
     const popover = await this.popoverController.create({
       component: PopoverComponent,
@@ -19,6 +21,7 @@ export class Tab2Page {
     });
     return await popover.present();
   }
+  /*
 =======
   constructor(private router:Router){}
 >>>>>>> 4e2c41cc4b63f454ebeca126d13a0a5f6707e27a
